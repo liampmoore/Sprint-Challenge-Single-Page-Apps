adfdd1e33a6d6c00e39b41cde32d981fcb636e15
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-01-31 12:41:46 -0500

    Implemented no results indicator on character list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Route} from 'react-router-dom';
+import axios from 'axios';
 
 import Header from "./components/Header.js";
 import WelcomePage from './components/WelcomePage';
@@ -9,15 +10,20 @@ import CharacterList from "./components/CharacterList.js";
 export default function App() {
 
   const [characters, setCharacters] = useState([]);
-  const [query, setQuery] = useState('');
+  const [charactersQuery, setCharactersQuery] = useState('');
+  const [charactersPage, setCharactersPage] = useState(1);
+
   useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character/')
+    axios.get(`https://rickandmortyapi.com/api/character/${charactersQuery ? `?name=${charactersQuery}`:''}${charactersQuery ? `&page=${charactersPage}`: ''}`)
       .then((res) => {
         console.log(res.data.results)
         setCharacters(res.data.results)
       })
-      .catch(err => console.log(err))
-  }, [query]);
+      .catch(err => {
+        console.log(err)
+        setCharacters([])
+      })
+  }, [charactersQuery, charactersPage]);
 
   return (
     <main>
@@ -26,7 +32,7 @@ export default function App() {
         <WelcomePage  />
       </Route>
       <Route path='/characters'>
-      <CharacterList characters={characters} setCharacters={setCharacters} query={query} setQuery={setQuery}/>
+      <CharacterList characters={characters} charactersQuery={charactersQuery} setCharactersQuery={setCharactersQuery}/>
       </Route>
     </main>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,16 @@
 import React from "react";
 
+import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
 
-export default function CharacterList({characters, setCharacters, query, setQuery}) {
-  // TODO: Add useState to track data from useEffect
+export default function CharacterList({characters, charactersQuery, setCharactersQuery}) {
   
+
 
   return (
     <section className="character-list">
+      <SearchForm query={charactersQuery} setQuery={setCharactersQuery}/>
+      {characters.length > 0 || <p>No results</p>}
       {characters.map((character) => {
         return (
           <CharacterCard key={character.id} character={character} />
