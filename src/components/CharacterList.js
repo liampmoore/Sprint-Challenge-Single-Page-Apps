69ecd92e2a79399f1f1fb0663c02763ead54a52a
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-01-31 11:25:38 -0500

    Got an initial list of character names displaying with an axios call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then((res) => {
+        console.log(res.data.results)
+        setCharacters(res.data.results)
+      })
+      .catch(err => console.log(err))
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map((character) => {
+        return (
+          <CharacterCard key={character.id} character={character} />
+        )
+      })}
     </section>
   );
 }
